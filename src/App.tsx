9932b28cc0703694fc7e54a5322c5fa18a6a2b0a
JanielleOliveira <janielleoliver.brito@gmail.com>
2025-10-31 15:26:11 -0300

    Criando os Componentes Card Postagem e Lista Postagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
 import FormTema from './components/tema/formtema/FormTema'
 import ListaTemas from './components/tema/listatemas/ListaTemas'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import DeletarTema from './components/tema/deletartema/DeletarTema'
+import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
 
 // Componente principal da aplicação
 function App() {
@@ -28,6 +29,7 @@ function App() {
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
 							<Route path="/deletartema/:id" element={<DeletarTema />} />
+							<Route path="/postagens" element={<ListaPostagens />} />
 						</Routes>
 					</div>
 				<Footer/>
